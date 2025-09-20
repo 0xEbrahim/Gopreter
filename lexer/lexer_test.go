611cc8f14523d7ada549c23b7b62b1ddcee69fc4
0xEbrahim <elsayedebrahim603@gmail.com>
2025-09-20 14:39:12 +0300

    test: tested simple lexer and worked well

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -24,7 +24,15 @@ func TestNextToken(t *testing.T) {
 
 	l := New(input)
 
-	for _, tt := range l.input {
-
+	for i, tt := range tests {
+		tok := l.NextToken()
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
+		if string(tok.Literal) != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
 	}
 }
